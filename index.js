f1f81f6d14aad142f2b7b29b29128b6ec01762e0
Yuyi <yuyi365@gmail.com>
2021-10-13 23:12:47 -0400

    commit tree fbbb6cc82c8db1609ce7a18320daf1115f2cc1f6

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'john'
+
+// function upperCaseCustomerName () {
+//     customerName = customerName.toUpperCase();
+// }
+
+var upperCaseCustomerName = () => {
+    customerName = customerName.toUpperCase();
+}
+
+var setBestCustomer = () => {
+    bestCustomer = 'not bob';
+}
+
+var overwriteBestCustomer = () => {
+    bestCustomer = 'maybe bob';
+}
+
+var changeLeastFavoriteCustomer = () => {
+    leastFavoriteCustomer = 'jane';
+}
